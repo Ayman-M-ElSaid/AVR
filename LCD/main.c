8f235e777c250ef20dd5744222ac26cc60d5afa9
Ayman-M-ElSaid <aymanelsaid@std.mans.edu.eg>
2026-03-16 16:26:40 +0200

    Display message on LCD
    Main function to display 'Eid Mubarak!' on LCD in a loop.

--- LCD/main.c
+++ LCD/main.c
@@ -5,13 +5,17 @@ int main()
   LCD lcd;
   lcd_init_4bit(&lcd, 'A');
 
-  lcd_goto(&lcd, 0, 0);
-  lcd_print(&lcd, "Hello");
-  lcd_goto(&lcd, 1, 0);
-  lcd_print(&lcd, "World!");
-
   while (1)
   {
+    for (uint8_t column = 0; column <= 16; column++)
+    {
+      lcd_clear(&lcd);
+      lcd_goto(&lcd, 0, column);
+      lcd_print(&lcd, "Eid");
+      lcd_goto(&lcd, 1, column + 1);
+      lcd_print(&lcd, "Mubarak!");
+      _delay_ms(100);
+    }
   }
   return 0;
 }
