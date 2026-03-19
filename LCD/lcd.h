543399e21aa636a81e52e975196c5b7f6d79ebf2
Ayman-M-ElSaid <aymanelsaid@std.mans.edu.eg>
2026-03-20 01:34:18 +0200

    Refactor LCD header for I2C support and new functions

--- LCD/lcd.h
+++ LCD/lcd.h
@@ -1,23 +1,28 @@
 #ifndef LCD_H_
 #define LCD_H_
 
-#include <avr\io.h>
-#include <avr\delay.h>
+#include <stdbool.h>
+#include <avr/io.h>
+#include <util/delay.h>
 
 typedef struct
 {
+    uint8_t MODE;
     volatile uint8_t *port;
     volatile uint8_t *ddr;
     uint8_t E;
     uint8_t RS;
-
+    uint8_t I2C_address;
 } LCD;
 
 void lcd_init_4bit(LCD *lcd, char port_name);
-void lcd_command(LCD *lcd, unsigned char cmd);
-void lcd_data(LCD *lcd,unsigned char data);
+void lcd_write(LCD *lcd, bool is_data, uint8_t byte);
+
+void lcd_init_I2C(LCD *lcd, uint8_t address);
+void lcd_I2C_write(LCD *lcd, bool is_data, uint8_t byte);
+
 void lcd_clear(LCD *lcd);
-void lcd_print(LCD *lcd,char *string);
+void lcd_print(LCD *lcd, char *string);
 void lcd_goto(LCD *lcd, uint8_t row, uint8_t col);
 
 #endif
